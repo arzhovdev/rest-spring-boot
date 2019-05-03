e6b4b3f38ab6f7dbad8119a6105a86e5b0e0f8d5
Hovhannes Arzumanyan <hovhannesa@energizeglobal.com>
2019-05-03 13:42:46 +0400

    init project

---
+++ src/main/java/com/arzhov/rest/ComArzhovRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.arzhov.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComArzhovRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComArzhovRestSpringBootApplication.class, args);
+	}
+
+}
